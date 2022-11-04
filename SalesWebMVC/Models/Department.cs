da161412efbc3b4222d48f05795370a9179d750d
Geovani Visentin <visentingeovani@gmail.com>
2022-11-04 14:18:05 -0300

    Department

---
+++ SalesWebMVC/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SalesWebMVC.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public static implicit operator List<object>(Department v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
